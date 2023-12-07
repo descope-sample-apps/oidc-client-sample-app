9949369a067894c8ee7e5ab663f99263cdf231f9
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-12-07 10:52:08 -0800

    Add check for JWT expiration

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+import { jwtDecode } from 'jwt-decode';
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
@@ -5,12 +6,13 @@ export function middleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
   let access_token = request.cookies.get('access_token')?.value;
 
-  // In production you should validate the access token here.
+  // In production you should validate the access token here (signature, etc).
+  // We simply check the expiration date
 
   if (pathName === '/' && access_token) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
-  if (pathName === '/dashboard' && !access_token) {
+  if (pathName === '/dashboard' && (!access_token || isJwtExpired(access_token))) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
@@ -28,4 +30,11 @@ export const config = {
          */
         '/((?!api|_next/static|_next/image|favicon.ico).*)',
       ],
+}
+
+const isJwtExpired = (token: string) => {
+    if (typeof token !== 'string' || !token) throw Error('Invalid token provided')
+    const { exp } = jwtDecode(token);
+    const currentTime = new Date().getTime() / 1000;
+    return exp && exp < currentTime;
 }
